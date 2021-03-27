8adcdb094e177ee683e1ad5f0278ad18de4c1609
aditya singh <adityasingh27@hotmail.com>
2021-03-27 21:37:05 +0530

    sawtooth react component

---
+++ web/index.js
@@ -0,0 +1,30 @@
+import React ,  { useState, useEffect } from 'react';
+
+
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+
+
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+function App(props) {
+
+  return (
+        <div>
+		</div>
+  );
+}
+
+
+
+
+export default App
